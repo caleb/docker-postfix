f869aa97bace6da507c4979f49092f4239514b7f
Caleb Land <caleb@land.fm>
2015-03-18 02:49:33 -0400

    wip

--- 2.11/docker-entrypoint.sh
+++ 2.11/docker-entrypoint.sh
@@ -15,6 +15,15 @@ export MAIN_MYNETWORKS
 export MAIN_SMTPD_BANNER
 export MAIN_DEBUGGER_COMMAND
 export MAIN_INET_PROTOCOLS
+export MAIN_SMTPD_HELO_RESTRICTIONS
+export MAIN_SMTPD_RECIPIENT_RESTRICTIONS
+export MAIN_SMTPD_SASL_AUTH_ENABLE
+
+# Ensure the domain or hostname is set
+if [ -z "${MAIN_MYDOMAIN}" ] && [ -z "${MAIN_MYHOSTNAME}" ]; then
+  echo "You must set either the MAIN_MYDOMAIN or MAIN_MYHOSTNAME variables" >&2
+  exit 1
+fi
 
 # Set up some default variables
 : ${MAIN_MYORIGIN:='$mydomain'}
@@ -36,8 +45,24 @@ if [ -n "${MAIN_MYHOSTNAME}" ] && [ -z "${MAIN_MYDOMAIN}" ]; then
 Setting mydomain to \$myhostname because \$myhostname contains only two components
 (${MYHOSTNAME}) which would result in a default mydomain of "${BASH_REMATCH[1]}".
 EOF
-    MAIN_MYDOMAIN='$myhostname'
+    export MAIN_MYDOMAIN="${MAIN_MYHOSTNAME}"
+  else
+    domain="$(echo "${MAIN_MYHOSTNAME}" | cut -d '.' -f 1 --complement)"
+    export MAIN_MYDOMAIN="${domain}"
   fi
+elif [ -z "${MAIN_MYHOSTNAME}" ] && [ -n "${MAIN_MYDOMAIN}" ]; then
+  # if the domain is set but not the hostname, set the hostname to the domain name
+  export MAIN_MYHOSTNAME="${MAIN_MYDOMAIN}"
+fi
+
+#
+# If opendkim is linked, set that up
+#
+if [ -n "${OPENDKIM_ADDR}" ] && [ -n "${OPENDKIM_PORT}" ]; then
+  export MAIN_MILTER_DEFAULT_ACTION=accept
+  export MAIN_MILTER_PROTOCOL=2
+  export MAIN_SMTPD_MILTERS=inet:${OPENDKIM_ADDR}:${OPENDKIM_PORT}
+  export MAIN_NON_SMTPD_MILTERS=inet:${OPENDKIM_ADDR}:${OPENDKIM_PORT}
 fi
 
 if [ ! -f /etc/postfix/main.cf ]; then
@@ -53,103 +78,23 @@ for template in /etc/postfix/**/*.mo; do
   rm "${template}"
 done
 
+#
+# Add users
+#
+if [ -n "${USER}" ]; then
+  export USER__DEFAULT__="${USER}"
+fi
 
-# # add domain
-# postconf -e myhostname="$1"
-# postconf -e mydestination="$1"
-# echo "$1" > /etc/mailname
-# echo "Domain $1" >> /etc/opendkim.conf
-
-# if [ ${#@} -gt 1 ]
-# then
-#   echo ">> adding users..."
-
-#   # all arguments but skip first argumenti
-#   i=0
-#   for ARG in "$@"
-#   do
-#     if [ $i -gt 0 ] && [ "$ARG" != "${ARG/://}" ]
-#     then
-#       USER=`echo "$ARG" | cut -d":" -f1`
-#       echo "    >> adding user: $USER"
-#       useradd -s /bin/bash $USER
-#       echo "$ARG" | chpasswd
-#       if [ ! -d /var/spool/mail/$USER ]
-#       then
-#         mkdir /var/spool/mail/$USER
-#       fi
-#       chown -R $USER:mail /var/spool/mail/$USER
-#       chmod -R a=rwx /var/spool/mail/$USER
-#       chmod -R o=- /var/spool/mail/$USER
-#     fi
-
-#     i=`expr $i + 1`
-#   done
-# fi
-
-# # DKIM
-# if [ -z ${DISABLE_DKIM+x} ]
-# then
-#   echo ">> enable DKIM support"
-
-#   if [ -z ${DKIM_CANONICALIZATION+x} ]
-#   then
-#     DKIM_CANONICALIZATION="simple"
-#   fi
-
-#   echo "Canonicalization $DKIM_CANONICALIZATION" >> /etc/opendkim.conf
-
-#   postconf -e milter_default_action="accept"
-#   postconf -e milter_protocol="2"
-#   postconf -e smtpd_milters="inet:localhost:8891"
-#   postconf -e non_smtpd_milters="inet:localhost:8891"
-
-#   # add dkim if necessary
-#   if [ ! -f /etc/postfix/dkim/dkim.key ]
-#   then
-#     echo ">> no dkim.key found - generate one..."
-#     opendkim-genkey -s mail -d $1
-#     mv mail.private /etc/postfix/dkim/dkim.key
-#     echo ">> printing out public dkim key:"
-#     cat mail.txt
-#     mv mail.txt /etc/postfix/dkim/dkim.public
-#     echo ">> please at this key to your DNS System"
-#   fi
-#   echo ">> change user and group of /etc/postfix/dkim/dkim.key to opendkim"
-#   chown opendkim:opendkim /etc/postfix/dkim/dkim.key
-#   chmod o=- /etc/postfix/dkim/dkim.key
-# fi
-
-# # add aliases
-# > /etc/aliases
-# if [ ! -z ${ALIASES+x} ]
-# then
-#   IFS=';' read -ra ADDR <<< "$ALIASES"
-#   for i in "${ADDR[@]}"; do
-#     echo "$i" >> /etc/aliases
-#     echo ">> adding $i to /etc/aliases"
-#   done
-# fi
-# echo ">> the new /etc/aliases file:"
-# cat /etc/aliases
-# newaliases
-
-# # starting services
-# echo ">> starting the services"
-# service rsyslog start
-
-# if [ -z ${DISABLE_DKIM+x} ]
-# then
-#   service opendkim start
-# fi
-
-# service saslauthd start
-# service postfix start
-
-# # print logs
-# echo ">> printing the logs"
-# touch /var/log/mail.log /var/log/mail.err /var/log/mail.warn
-# chmod a+rw /var/log/mail.*
-# tail -F /var/log/mail.*
+for var in ${!USER_*}; do
+  if [[ "${!var}" =~ ^([^:]+):(.*)$ ]]; then
+    username="${BASH_REMATCH[1]}"
+    password="${BASH_REMATCH[2]}"
+    echo "Creating user \"${username}\" with password \"${password}\""
+    echo "${password}" | saslpasswd2 -p -c -u "${MAIN_MYDOMAIN}" "${username}"
+  else
+    echo "Misformed user variable ${var}=${!var}. Should be ${var}=username:password" >&2
+    exit 1
+  fi
+done
 
 exec "${@}"
