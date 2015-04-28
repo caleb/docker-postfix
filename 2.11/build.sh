5a750de1de5cbffd35bd7cd310383763ebb79614
Caleb Land <caleb@land.fm>
2015-04-28 17:53:47 -0400

    add a flag to skip caches when building images

--- 2.11/build.sh
+++ 2.11/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --tag=docker.rodeopartners.com/postfix:2.11 .
+NO_CACHE=${1:-false}
+
+docker build --no-cache=$NO_CACHE --tag=docker.rodeopartners.com/postfix:2.11 .
 docker tag -f docker.rodeopartners.com/postfix:2.11 docker.rodeopartners.com/postfix:2.11-jessie
--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+NO_CACHE=${1:-false}
+
 cd 2.11
-./build.sh
+./build.sh $NO_CACHE
 cd ..
 docker tag -f docker.rodeopartners.com/postfix:2.11 docker.rodeopartners.com/postfix:latest
